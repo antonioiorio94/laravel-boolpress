fdc6ab786e03bd46533433d2e4a4a06be9c9fbde
Antonio Iorio <antonioiorio94@gmail.com>
2022-06-09 16:43:04 +0200

    install vue router

--- public/js/front-app.js
+++ public/js/front-app.js
@@ -49862,28 +49862,11 @@ window.axios.get("http://127.0.0.1:8000/api/posts").then(function (results) {
   console.log(e);
 });
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
 
 var app = new Vue({
   el: "#app",
-  render: function render(h) {
-    return h(_app_AppComponent__WEBPACK_IMPORTED_MODULE_0__["default"]);
+  render: function render(createElement) {
+    return createElement(_app_AppComponent__WEBPACK_IMPORTED_MODULE_0__["default"]);
   }
 });
 
